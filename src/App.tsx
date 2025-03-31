b1d12b6575a47cea0ff0f57923d4c1160c9c3467
NikolaSirakov2 <nikolasirakov1@gmail.com>
2025-04-01 00:20:11 +0800

    Add Tailwind CSS theming and utility functions; introduce Button component; update package dependencies and TypeScript configuration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import { Button } from '@/components/ui/button'
 
 function App() {
 
   return (
     <>
       <div className='flex justify-center items-center h-screen w-screen bg-blue-500'>
-        <h1 className='text-6xl font-bold'>Kikimora</h1>
+        <div className='flex flex-col justify-center items-center'>
+          <h1 className='text-6xl font-bold'>Kikimora</h1>
+        </div>
+        <div className='flex justify-center items-center'>
+          <Button>Click me</Button>
+        </div>
       </div>
     </>
   )
