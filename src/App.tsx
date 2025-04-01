c9e49a602f82a6dcd1e82fde628e8a4d9f4e0007
NikolaSirakov2 <nikolasirakov1@gmail.com>
2025-04-01 10:15:48 +0800

    Refactor Tailwind CSS configuration to include custom font families and enhance color theming; add ConthraxSb font; update App component styling for consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
-
   return (
     <>
-      <div className='flex justify-center items-center h-screen w-screen bg-blue-500'>
-        <div className='flex flex-col justify-center items-center'>
-          <h1 className='text-6xl font-bold'>Kikimora</h1>
-        </div>
-        <div className='flex justify-center items-center'>
-          <Button>Click me</Button>
+      <div className="flex justify-center items-center h-screen w-screen bg-blue-500">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-6xl font-normal">Kikimora</h1>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
