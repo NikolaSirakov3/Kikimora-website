6773d1b59524337b8f01df505e746c0ed45ab5b2
NikolaSirakov2 <nikolasirakov1@gmail.com>
2025-05-27 09:26:08 +0300

    Update Journey component layout by adjusting timeline point positioning and removing unnecessary navigation arrows for improved visual clarity and simplicity.

--- src/components/about/Journey.tsx
+++ src/components/about/Journey.tsx
@@ -40,7 +40,7 @@ export function Journey() {
           {/* Timeline */}
           <div className="relative">
             {/* Timeline line */}
-            <div className="absolute left-0 right-0 h-[2px] bg-[#00E5BE]/30 top-[22px]" />
+            <div className="absolute left-0 right-0 h-[2px] bg-[#00E5BE]/30 top-[50px]" />
 
             {/* Timeline points */}
             <div className="relative grid grid-cols-6 gap-4">
@@ -51,8 +51,8 @@ export function Journey() {
                     index === 0
                       ? "items-start"
                       : index === timelineData.length - 1
-                      ? "items-end"
-                      : "items-center"
+                        ? "items-end"
+                        : "items-center"
                   }`}
                 >
                   {/* Year */}
@@ -61,41 +61,7 @@ export function Journey() {
                   </div>
 
                   {/* Point */}
-                  <div className="w-4 h-4 rounded-full bg-[#00E5BE] mb-4 relative">
-                    {/* Navigation arrows */}
-                    {index === 0 && (
-                      <div className="absolute -left-8 top-0 text-[#00E5BE]">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-4 w-4"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </div>
-                    )}
-                    {index === timelineData.length - 1 && (
-                      <div className="absolute -right-8 top-0 text-[#00E5BE]">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-4 w-4"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </div>
-                    )}
-                  </div>
+                  <div className="w-4 h-4 rounded-full bg-[#00E5BE] mb-4 relative"></div>
 
                   {/* Title */}
                   <div className="text-white font-montserrat text-sm text-center">
